04762fbc2c35ba5617dfc9fcb1c903233f9a86a4
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-30 18:14:22 -0600

    Update binarySerachTree.js

--- dataStructures/binarySerachTree.js
+++ dataStructures/binarySerachTree.js
@@ -55,13 +55,56 @@ class BinarySearchTree {
         return currentNode;
     }
   }
+
+  /**
+   * Breadth first search
+   * Going through tree horisontally by syblings
+   */
+  bfs() {
+    if (!this.root) return [];
+
+    const queue = [this.root],
+          visited = [];
+
+    while(queue.length) {
+      const node = queue.shift();
+      visited.push(node.value);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return visited;
+  }
+
+  /**
+   * Depth first search - PreOrder
+   * Going through tree vertically down
+   */
+  dfs() {
+    if (!this.root) return [];
+    const visited = [];
+
+    (function loop(node) {
+      visited.push(node.value);
+      if (node.left) loop(node.left);
+      if (node.right) loop(node.right);
+    })(this.root);
+
+    return visited;
+  }
 }
 
 const tree = new BinarySearchTree();
 
 tree.insert(10);
-tree.insert(11);
-tree.insert(9);
+tree.insert(6);
+tree.insert(3);
 tree.insert(8);
+tree.insert(15);
+tree.insert(20);
 
-console.log(JSON.stringify(tree, null, 2));
+// console.log(JSON.stringify(tree, null, 2));
+
+console.log('BFS:', tree.bfs());
+console.log('DFS:', tree.dfs());
